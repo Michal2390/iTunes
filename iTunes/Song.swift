a230cbc08f44b684d526d6d203337f774884e02e
Michał Fereniec <michalkoks99@gmail.com>
2022-07-27 16:05:04 +0200

    My first take on basic UI

--- iTunes/Song.swift
+++ iTunes/Song.swift
@@ -7,33 +7,39 @@
 import Foundation
 
 struct Song: Decodable {
-    let wrapperType: WrapperType
-    let kind: Kind?
-    let collectionID, trackID: Int?
-    let artistName: String
-    let collectionName, trackName, collectionCensoredName, trackCensoredName: String?
-    let collectionArtistID: Int?
-    let collectionArtistViewURL, collectionViewURL, trackViewURL: String?
-    let previewURL: String?
-    let artworkUrl30: String?
-    let artworkUrl60, artworkUrl100: String
-    let collectionPrice: Double
-    let trackPrice, trackRentalPrice, collectionHDPrice, trackHDPrice: Double?
-    let trackHDRentalPrice: Double?
-    let collectionExplicitness: Explicitness
-    let trackExplicitness: Explicitness?
-    let discCount, discNumber, trackCount, trackNumber: Int?
-    let trackTimeMillis: Int?
-    let country: Country
-    let currency: Currency
-    let primaryGenreName: String
-    let contentAdvisoryRating, shortDescription, longDescription: String?
-    let hasITunesExtras: Bool?
-    let artistID: Int?
-    let artistViewURL: String?
-    let isStreamable: Bool?
-    let feedURL: String?
-    let artworkUrl600: String?
-    let genreIDS, genres: [String]?
-    let resultDescription, copyright, collectionArtistName: String?
+//    let wrapperType: WrapperType
+//    let kind: Kind?
+//    let collectionID, trackID: Int?
+//    let artistName: String
+//    let collectionName, trackName, collectionCensoredName, trackCensoredName: String?
+//    let collectionArtistID: Int?
+//    let collectionArtistViewURL, collectionViewURL, trackViewURL: String?
+//    let previewURL: String?
+//    let artworkUrl30: String?
+//    let artworkUrl60, artworkUrl100: String
+//    let collectionPrice: Double
+//    let trackPrice, trackRentalPrice, collectionHDPrice, trackHDPrice: Double?
+//    let trackHDRentalPrice: Double?
+//    let collectionExplicitness: Explicitness
+//    let trackExplicitness: Explicitness?
+//    let discCount, discNumber, trackCount, trackNumber: Int?
+//    let trackTimeMillis: Int?
+//    let country: Country
+//    let currency: Currency
+//    let primaryGenreName: String
+//    let contentAdvisoryRating, shortDescription, longDescription: String?
+//    let hasITunesExtras: Bool?
+//    let artistID: Int?
+//    let artistViewURL: String?
+//    let isStreamable: Bool?
+//    let feedURL: String?
+//    let artworkUrl600: String?
+//    let genreIDS, genres: [String]?
+//    let resultDescription, copyright, collectionArtistName: String?
+    
+                   let name: String
+                   let albumName: String
+                    let artistName: String
+                    let imageName: String
+                   let trackName: String
 }
--- iTunes/ViewController.swift
+++ iTunes/ViewController.swift
@@ -7,10 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    @IBOutlet var table: UITableView!
+    
+    var songs = [Song]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         MusicService().cos()
+        configureSongs()
+        table.delegate = self
+        table.dataSource = self
+    }
+    func configureSongs() {
+        songs.append(Song(  name: "Leje się szampan",
+                            albumName: "Pozytywnie Kreatywni",
+                            artistName: "Prince PKS",
+                            imageName: "background1",
+                            trackName: "prince-pks-leje-sie-szampan"))
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songs.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let song = songs[indexPath.row]
+        
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.albumName
+        cell.accessoryType = .disclosureIndicator
+        cell.imageView?.image = UIImage(named: song.imageName)
+        cell.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 18)
+        cell.detailTextLabel?.font = UIFont(name: "Helvetica", size: 17)
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let position = indexPath.row
+        guard let vcc = storyboard?.instantiateViewController(withIdentifier: "player") else { return }
+        present(vcc, animated: true)
     }
 }
