69054367e2dad1c091e39c40fb1bfc170494236a
Michał Fereniec <michalkoks99@gmail.com>
2022-07-27 21:54:09 +0200

    small update to my last commit

--- iTunes/ViewController+UITableViewDataSource.swift
+++ iTunes/ViewController+UITableViewDataSource.swift
@@ -9,7 +9,19 @@ import Foundation
 import UIKit
 
 extension ViewController {
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let song = songs[indexPath.row]
+        
+        cell.textLabel?.text = song.name
+        cell.detailTextLabel?.text = song.albumName
+        cell.accessoryType = .disclosureIndicator
+        cell.imageView?.image = UIImage(named: song.imageName)
+        cell.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 18)
+        cell.detailTextLabel?.font = UIFont(name: "Helvetica", size: 17)
+        
+        return cell
+    }
     func configureSongs() {
         songs.append(Song(  name: "Leje się szampan",
                             albumName: "Pozytywnie Kreatywni",
--- iTunes/ViewController+UITableViewDelegate.swift
+++ iTunes/ViewController+UITableViewDelegate.swift
@@ -9,19 +9,6 @@ import Foundation
 import UIKit
 
 extension ViewController {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let song = songs[indexPath.row]
-        
-        cell.textLabel?.text = song.name
-        cell.detailTextLabel?.text = song.albumName
-        cell.accessoryType = .disclosureIndicator
-        cell.imageView?.image = UIImage(named: song.imageName)
-        cell.textLabel?.font = UIFont(name: "Helvetica-Bold", size: 18)
-        cell.detailTextLabel?.font = UIFont(name: "Helvetica", size: 17)
-        
-        return cell
-    }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let position = indexPath.row
